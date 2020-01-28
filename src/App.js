0cebff793af95f89899a8b23c698b701f7eee407
Meka <nmeka@deloitte.com>
2020-01-28 13:51:46 +0530

    props using person object

--- src/App.js
+++ src/App.js
@@ -2,20 +2,25 @@ import React, { Component } from "react";
 import "./App.css";
 import PropTypes from "prop-types";
 
-const Person = ({ img, name, age, info }) => {
+const Person = ({ person: { img, name, age, info } }) => {
+  //const { img, name, age, info } = props.person;
   return (
     <article>
       <img src={img} alt="person" />
       <h4>name : {name}</h4>
       <h4>age: {age}</h4>
-      <h4>info: {info}</h4>
+      <h4>info: {info || "default ifo about the person"}</h4>
     </article>
   );
 };
+//person is a object and getting values from person obj
 Person.propTypes = {
-  img: PropTypes.string.isRequired,
-  name: PropTypes.string.isRequired,
-  age: PropTypes.number.isRequired
+  person: PropTypes.shape({
+    img: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    age: PropTypes.number.isRequired,
+    info: PropTypes.string.isRequired
+  })
 };
 Person.defaultProps = {
   img: "https://randomuser.me/api/portraits/thumb/men/65.jpg",
@@ -51,13 +56,7 @@ class PersonList extends Component {
     return (
       <section>
         {this.state.people.map(person => (
-          <Person
-            key={person.id}
-            img={person.img}
-            name={person.name}
-            age={person.age}
-            info={person.info}
-          />
+          <Person key={person.id} person={person} />
         ))}
       </section>
     );
