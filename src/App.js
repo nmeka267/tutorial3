8f58399da9eba08c14afe7927eb933a44b75ece7
Meka <nmeka@deloitte.com>
2020-01-28 17:58:35 +0530

    un controlled inputs

--- src/App.js
+++ src/App.js
@@ -3,67 +3,29 @@ import "./App.css";
 //uncontrolled inputs
 
 class Form extends Component {
-  state = {
-    firstName: "",
-    lastName: "",
-    people: []
-  };
-  handleChange = event => {
-    // console.log(event.target);
-    // console.log(event.target.name);
-    // console.log(event.target.value);
-
-    // if (event.target.name === "firstName") {
-    //   const textValue = event.target.value;
-
-    //   this.setState({
-    //     firstName: textValue
-    //   });
-    // }
-    const value = event.target.value;
-    this.setState({
-      [event.target.name]: value
-    });
-  };
-
   handleSubmit = e => {
     e.preventDefault();
-    const firstName = this.state.firstName;
-    const lastName = this.state.lastName;
-    if (firstName.length > 0 && lastName.length > 0) {
-      const person = ` ${firstName} ${lastName} `;
-      this.setState({
-        people: [...this.state.people, person],
-        firstName: "",
-        lastName: ""
-      });
-    }
+    const name = this.refs.myName;
+    console.log(name);
+    const nameValue = name.value;
+    const text = this.refs.myText;
+    text.style.color = "red";
+    console.log(nameValue, email, text);
   };
-
   render() {
     return (
       <section>
-        <article>
-          <form onSubmit={this.handleSubmit}>
-            <input
-              type="text"
-              name="firstName"
-              value={this.state.firstName}
-              onChange={this.handleChange}
-            />
-            <input
-              type="text"
-              name="lastName"
-              value={this.state.lastName}
-              onChange={this.handleChange}
-            />
-            <button type="submit">submit</button>
-          </form>
-        </article>
-        <article>
-          <h1>people</h1>
-          <div>{this.state.people}</div>
-        </article>
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" ref="myName" />
+          <input type="email" ref={apple => (this.email = apple)} />
+          <button
+            type="
+          submit"
+          >
+            submit
+          </button>
+          <p>Hello uncontrolled inputss</p>
+        </form>
       </section>
     );
   }
